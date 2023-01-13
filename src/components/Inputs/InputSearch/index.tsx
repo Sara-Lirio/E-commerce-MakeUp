a29c22c9a563880172e862278ea6ba67806bbe8b
Sara <saraeliza.lirio@gmail.com>
2023-01-13 14:36:56 -0300

    Componente - Input de Busca

---
+++ src/components/Inputs/InputSearch/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+import { VscSearch } from "react-icons/vsc";
+
+export interface InputProps {
+  placeholder?: string,
+}
+
+const Fieldset = styled.fieldset`
+  border: .1em solid var(--cinza-color);
+  border-radius: .4em;
+  padding: .2em 0 .2em 1em;
+  width: 34em;
+  height:2em;
+  font-family: var(--font-overpass);
+  color: var(--grafite-color);
+  display:flex;
+  justify-content: center;
+  align-items: center;
+
+  input{
+    border:none;
+    width: 30em;
+    outline:none;
+    font-size:.9em;
+    color: var(--grafite-color);
+    font-family: var(--font-overpass);
+  }
+`
+
+const InputSearch = ({placeholder}:InputProps) => {
+  return (
+    <Fieldset>
+      <input placeholder={placeholder}/>
+      <VscSearch/>
+    </Fieldset>
+  )
+}
+
+export default InputSearch
