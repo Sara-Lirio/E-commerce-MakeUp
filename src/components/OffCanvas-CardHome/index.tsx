58a203e90b192fd877e335ae66fb5b2e2a602796
Sara <saraeliza.lirio@gmail.com>
2023-01-24 15:46:39 -0300

    Adicionando a funcionalidade de fechar o Carrinho

--- src/components/OffCanvas-CardHome/index.tsx
+++ src/components/OffCanvas-CardHome/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
 import styled from 'styled-components'
-
+import ButtonSecondary from '../Buttons/secondary'
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const StyledOffCanvas = styled.section`
     color: var(--grafite-color);
     font-family: var(--font-overpass);
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    
+    .buttonClose {
+      border:none;
+      background-color:transparent;
+      margin-left: 20em;
+    }
 
     h2 {
       font-weigth: 400;
@@ -15,14 +24,35 @@ const StyledOffCanvas = styled.section`
       border-bottom: 1px solid var(--cinza-gelo);
       margin: 1em;
     }
+
+    .offcanvas-header {
+      margin:0
+    }
+
+    .btn-close {
+      margin-left: 20em;
+      margin-top: .5em
+    }
+
+    div {
+      text-align: center;
+      margin: 3em;
+    }
 `
 
 const OffCanvasHome = () => {
   return (
     <StyledOffCanvas>
-    <header>
+      <Offcanvas.Header closeButton>
+    
+      </Offcanvas.Header>
       <h2>Meu Carrinho</h2>
-    </header>
+      <div>
+        <p>VOCÊ NÃO TEM NENHUM PRODUTO NO CARRINHO<br /><br />
+          Quando você escolher seus produtos, mostraremos tudo aqui.</p>
+      </div>
+      <ButtonSecondary text='Ver carrinho' />
+
     </StyledOffCanvas>
   )
 }
