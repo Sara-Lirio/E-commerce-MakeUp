4b2268803e0339dbd6f1c0f7b5301672734ee288
Sara <saraeliza.lirio@gmail.com>
2023-01-25 16:41:16 -0300

    Inserindo sub abas no menu do header

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -35,12 +35,33 @@ const ContainerMenu = styled.div`
 
 .dropdown-menu.show {
     background-color: var(--grafite-color);
-    
+    height: 15em;
+    width: 24em;
+    display:flex;
+    padding: 1.3em 2em;
+    flex-direction: column;
+    align-items: center;
+    margin-top: .3em;
+}
+
+.makes {
+    display:flex;
+    width: 100%;
+    justify-content: space-between
+}
+
+.dropdown-divider{
+    border-top: 1px solid var(--gelo-color);
+    width: 18em
+}
+
+.verTudo{
+    text-align: center;
 }
 
 .dropdown-item {
     color: var(--gelo-color);
-    font-size: 12px;
+    font-size: 13px;
 }
 
 .dropdown-item:hover {
@@ -53,21 +74,55 @@ const Menu = () => {
         <ContainerMenu>
             <ul>
                 <NavDropdown title="Maquiagem" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Blush</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Bronzer</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Eyebrow</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">Eyeliner</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.5">Eyeshadow</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.6">Foundation</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.7">Lip liner</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.8">Lipstick</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.9">Mascara</NavDropdown.Item>
-                   <NavDropdown.Divider />
+                    <section className='makes'>
+                        <div className='column1'>
+                            <NavDropdown.Item href="/blush">Blush</NavDropdown.Item>
+                            <NavDropdown.Item href="/bronzer">Bronzer</NavDropdown.Item>
+                            <NavDropdown.Item href="/eyebrow">Eyebrow</NavDropdown.Item>
+                            <NavDropdown.Item href="/delineadores">Delineadores</NavDropdown.Item>
+                            <NavDropdown.Item href="/sombras">Sombras</NavDropdown.Item>
+                            <NavDropdown.Item href="/corretivos">Corretivos</NavDropdown.Item>
+                        </div>
+                        <div className='column2'>
+                            <NavDropdown.Item href="/bases">Base</NavDropdown.Item>
+                            <NavDropdown.Item href="/pomineral">Pó Mineral</NavDropdown.Item>
+                            <NavDropdown.Item href="/lapisboca">Lápis de Boca</NavDropdown.Item>
+                            <NavDropdown.Item href="/batons">Batons</NavDropdown.Item>
+                            <NavDropdown.Item href="/gloss">Gloss</NavDropdown.Item>
+                            <NavDropdown.Item href="/mascaras">Mascaras</NavDropdown.Item>
+                        </div>
+                    </section>
+                    <section className='verTudo'>
+                    <NavDropdown.Divider />
                     <NavDropdown.Item href="/produtos">Ver tudo</NavDropdown.Item>
+                    </section>
                 </NavDropdown>|
                 <li>Esmaltes</li>|
                 <li>Promos</li>|
-                <li>Marcas</li>
+                <NavDropdown title="Marcas" id="basic-nav-dropdown">
+                    <section className='makes'>
+                        <div className='column1'>
+                            <NavDropdown.Item href="/blush">Almay</NavDropdown.Item>
+                            <NavDropdown.Item href="/bronzer">Alva</NavDropdown.Item>
+                            <NavDropdown.Item href="/eyebrow">Anna Sui</NavDropdown.Item>
+                            <NavDropdown.Item href="/delineadores">Annabelle</NavDropdown.Item>
+                            <NavDropdown.Item href="/sombras">Benefit</NavDropdown.Item>
+                            <NavDropdown.Item href="/corretivos">Boosh</NavDropdown.Item>
+                        </div>
+                        <div className='column2'>
+                            <NavDropdown.Item href="/bases">Burt's Bees</NavDropdown.Item>
+                            <NavDropdown.Item href="/pomineral">Butter London</NavDropdown.Item>
+                            <NavDropdown.Item href="/lapisboca">C'est moi</NavDropdown.Item>
+                            <NavDropdown.Item href="/batons">Cargo Cosmetics</NavDropdown.Item>
+                            <NavDropdown.Item href="/gloss">China Glaze</NavDropdown.Item>
+                            <NavDropdown.Item href="/mascaras">Clinique</NavDropdown.Item>
+                        </div>
+                    </section>
+                    <section className='verTudo'>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/produtos">Ver mais</NavDropdown.Item>
+                    </section>
+                </NavDropdown>
             </ul>
         </ContainerMenu>
     )
