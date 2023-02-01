a048b882bca059b0ec1809047d94f9b651a892ca
Sara <saraeliza.lirio@gmail.com>
2023-02-01 13:58:54 -0300

    Acrescentado atributos no Type de Produto

--- src/view/types/Product.ts
+++ src/view/types/Product.ts
@@ -1,8 +1,16 @@
 export type Product = {
+    id?: number;
     image_link: string;
     category: string;
     name: string;
     price: number;
     brand?: string;
     product_type?: string;
+    description?: string;
+    price_sign ?: string;
+    product_colors ?: Array<Color>
+}
+interface Color {
+    hex_value: string;
+    colour_name: string
 }
