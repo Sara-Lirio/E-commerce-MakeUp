5bd3ac9dd51723d4fd6a21548b2876bb537aa872
Sara <saraeliza.lirio@gmail.com>
2023-02-01 13:59:20 -0300

    Organizando as rotas de produtos

--- src/view/routes/AppRoutes.tsx
+++ src/view/routes/AppRoutes.tsx
@@ -6,8 +6,8 @@ import Header from '../components/Header'
 import Menu from '../components/Menu'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
-import Product from '../pages/Product'
-import ProductsPage from '../pages/Products'
+import ProductId from '../pages/Product'
+import ProductsType from '../pages/Product_Type'
 import Registration from '../pages/Registration'
 import ShoppingCart from '../pages/ShoppingCart'
 
@@ -20,9 +20,9 @@ const AppRoutes = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/cadastre-se' element={<Registration/>} />
-        <Route path='/produtos' element={<ProductsPage/>} />
+        <Route path='/products' element={<ProductsType/>} />
         <Route path='/shopping' element={<ShoppingCart/>} />
-        <Route path='/product' element={<Product/>} />
+        <Route path='/products/:id' element={<ProductId/>} />
       </Routes>
         <Footer/>
     </BrowserRouter>
